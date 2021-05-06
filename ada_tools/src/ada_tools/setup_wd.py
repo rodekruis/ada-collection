a0ce98afb01212d21efd6b53b2d00d2721ad5904
jmargutti <jacopo.margutti@gmail.com>
2021-05-06 16:48:40 +0200

    split and merge raster mosaic

--- ada_tools/src/ada_tools/setup_wd.py
+++ ada_tools/src/ada_tools/setup_wd.py
@@ -15,6 +15,7 @@ import sys
 from typing import Callable, List, NamedTuple
 import rasterio
 from rasterio.enums import Resampling
+from rasterio.merge import merge
 import numpy as np
 from tqdm import tqdm
 
@@ -71,8 +72,6 @@ def create_raster_mosaic(
     src_files = [os.path.join(data, name) for name in filenames]
     out_file = os.path.join(data, "merged.tif")
 
-    rasters = []
-
     # The array size (out_shape) will be taken from the first image, along with a
     # corresponding profile (the set of metadata particular to that image) and. 
     # `transform` refers to an affine transformation matrix mapping pixel coordinates to
@@ -81,49 +80,97 @@ def create_raster_mosaic(
     profile = None
     transform = None
 
-    for path in tqdm(src_files, desc="Reading tif files"):
-        with rasterio.open(path, "r") as f:
-            # Calculate the boundary of the tile in pixel coordinates.
-            window =f.window(
-                tile.left, tile.bottom, tile.right, tile.top
-            )
+    windows = [
+        [tile.left, (tile.top+tile.bottom)/2, (tile.right+tile.left)/2, tile.top],
+        [(tile.right+tile.left)/2, (tile.top+tile.bottom)/2, tile.right, tile.top],
+        [tile.left, tile.bottom, (tile.right+tile.left)/2, (tile.top+tile.bottom)/2],
+        [(tile.right+tile.left)/2, tile.bottom, tile.right, (tile.top+tile.bottom)/2]
+    ]
+    add_out_file=False
+    mosaics = []
+    mosaics_path = []
 
-            # Read image data from the given window.
-            # When `boundless` is True, parts of the window that fall outside of the
-            # raster are given the `fill_value` value; this gives us consistently-sized
-            # arrays to work with. When the image's dtype is uint8, which Maxar images
-            # generally seem to be, there is no available fill_value that couldn't also
-            # be a valid pixel value. Therefore we read it as a float and set the
-            # fill_value to np.nan so that we can easily disregard them when generating
-            # the mosaic.
-            #
-            # The images also tend to have slight differences in resolution, leading to
-            # different array sizes. With the `out_shape` explicitly set, it rescales
-            # the image to fit that shape.
-            raster = f.read(
-                window=window,
-                boundless=True,
-                out_shape=out_shape,
-                fill_value=np.nan,
-                out_dtype=np.float32,
-                resampling=Resampling.lanczos,
-            )
+    for ix in tqdm(range(len(src_files)), desc="Reading tif files"):
+        if ix == 0:
+            src_files_ = src_files[:2]
+            add_out_file = False
+        elif ix == 1:
+            continue
+        else:
+            src_files_ = [src_files[ix]]
+            add_out_file = True
 
-            if out_shape is None:
-                out_shape = raster.shape
-                profile = f.profile
-                transform = f.window_transform(window)
-            rasters.append(raster)
+        rasters = {0: [], 1: [], 2: [], 3: []}
 
-    # create the mosaic and convert it from float back to the original dtype
-    mosaic = agg(np.stack(rasters, axis=0))
-    mosaic = mosaic.astype(profile["dtype"])
+        for path in src_files_:
+            with rasterio.open(path, "r") as f:
+                # Calculate the boundary of the tile in pixel coordinates.
+                # window =f.window(
+                #     tile.left, tile.bottom, tile.right, tile.top
+                # )
 
-    # update the profile with the new shape and affine transform
-    profile.update(height=mosaic.shape[1], width=mosaic.shape[2], transform=transform)
+                for num_wind, wind_extr in enumerate(windows):
+                    window = f.window(wind_extr[0], wind_extr[1], wind_extr[2], wind_extr[3])
+                    # Read image data from the given window.
+                    # When `boundless` is True, parts of the window that fall outside of the
+                    # raster are given the `fill_value` value; this gives us consistently-sized
+                    # arrays to work with. When the image's dtype is uint8, which Maxar images
+                    # generally seem to be, there is no available fill_value that couldn't also
+                    # be a valid pixel value. Therefore we read it as a float and set the
+                    # fill_value to np.nan so that we can easily disregard them when generating
+                    # the mosaic.
+                    #
+                    # The images also tend to have slight differences in resolution, leading to
+                    # different array sizes. With the `out_shape` explicitly set, it rescales
+                    # the image to fit that shape.
+                    raster = f.read(
+                        window=window,
+                        boundless=True,
+                        out_shape=out_shape,
+                        fill_value=np.nan,
+                        out_dtype=np.float32,
+                        resampling=Resampling.lanczos,
+                    )
 
-    with rasterio.open(out_file, "w", **profile) as f:
-        f.write(mosaic)
+                    if out_shape is None:
+                        out_shape = raster.shape
+                        profile = f.profile
+                        transform = f.window_transform(window)
+                    rasters[num_wind].append(raster)
+                    # rasters_name[num_wind].append(path)
+
+                    if add_out_file:
+                        rasters[num_wind].append(mosaics[num_wind])
+                        # rasters_name[num_wind].append(out_file.replace('.tif', f'-{num_wind}.tif'))
+
+        # create the mosaic and convert it from float back to the original dtype
+        # print(f"iteration {ix}, rasters {rasters_name[0]}")
+        for num_wind in rasters.keys():
+            mosaic = agg(np.stack(rasters[num_wind], axis=0))
+            mosaic = mosaic.astype(profile["dtype"])
+
+            # update the profile with the new shape and affine transform
+            profile.update(height=mosaic.shape[1], width=mosaic.shape[2], transform=transform)
+
+            with rasterio.open(out_file.replace('.tif', f'-{num_wind}.tif'), "w", **profile) as f:
+                f.write(mosaic)
+            if ix == 0:
+                mosaics_path.append(out_file.replace('.tif', f'-{num_wind}.tif'))
+                with rasterio.open(out_file.replace('.tif', f'-{num_wind}.tif'), "r") as f:
+                    mosaics.append(f.read(
+                            window=window,
+                            boundless=True,
+                            out_shape=out_shape,
+                            fill_value=np.nan,
+                            out_dtype=np.float32,
+                            resampling=Resampling.lanczos,
+                        ))
+
+    # merge all mosaics
+    os.system(r'gdalbuildvrt "{}" "{}"'.format(out_file.replace('.tif', '.vrt'), os.path.join(data, "merged*.tif")))
+    os.system(r'gdal_translate "{}" "{}"'.format(out_file.replace('.tif', '.vrt'), out_file))
+    # for path in mosaics_path:
+    #     os.remove(path)
 
 
 def get_tile(df: gpd.GeoDataFrame, id: str) -> Tile:
@@ -153,6 +200,9 @@ def main(data, index, id, dest):
     """
     index_df = gpd.read_file(index)
     tile = get_tile(index_df, id)
+    print(tile)
+    print(tile.left, tile.bottom, tile.right, tile.top)
+    print(index_df[index_df.tile == id].geometry.bounds)
 
     for image in tile.pre_event:
         img_path = os.path.expanduser(os.path.join(dest, 'pre-event'))
