aa1cb68ffc2fcbb05c1e4f4270850de26299603a
jmargutti <jacopo.margutti@gmail.com>
2023-02-11 15:42:38 +0100

    bug fix crs

--- ada_tools/random-scripts/abd-pipeline.py
+++ ada_tools/random-scripts/abd-pipeline.py
@@ -14,7 +14,7 @@ def main(dir, dest, config):
     for AOI in ['AOI3', 'AOI2', 'AOI4', 'AOI5']:
         dir = os.path.join(dir, AOI, 'pre-event')
         dest = os.path.join(dest, AOI)
-        subprocess.run(f"abd cover --raster {dir}/*.tif --zoom 17 --out {dest}/cover.csv", shell=True)
+        subprocess.run(f"abd cover --raster {dir}/*.tif --zoom 17 --out {dest}/cover.csv --crs EPSG:32630", shell=True)
         subprocess.run(f"abd tile --raster {dir}/*.tif --zoom 17 --cover {dest}/cover.csv --config {config}/config.toml --out {dest}/images --format tif --keep_borders", shell=True)
         subprocess.run(f"abd predict --cover {dest}/cover.csv --config {config}/config.toml --dataset {dest} --checkpoint {config}/neat-fullxview-epoch75.pth --out {dest}/predictions --metatiles --keep_borders", shell=True)
         subprocess.run(f"abd vectorize --config {config}/config.toml --masks {dest}/predictions --out {dest}/buildings.geojson --type Building", shell=True)
