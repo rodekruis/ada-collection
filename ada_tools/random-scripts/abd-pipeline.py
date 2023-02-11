131e6a6628675c997d795157767ea0a091adc8bd
jmargutti <jacopo.margutti@gmail.com>
2023-02-11 14:55:49 +0100

    add jp2tiff

---
+++ ada_tools/random-scripts/abd-pipeline.py
@@ -0,0 +1,25 @@
+# Importing required module
+import subprocess
+from pathlib import Path
+import os
+import click
+
+
+@click.command()
+@click.option('--dir', help='input directory')
+@click.option('--dest', help='output directory')
+@click.option('--config', help='config directory')
+def main(dir, dest, config):
+
+    for AOI in ['AOI1']:
+        dir = os.path.join(dir, AOI, 'pre-event')
+        dest = os.path.join(dest, AOI)
+
+    subprocess.run(f"abd cover --raster {dir}/*.tif --zoom 17 --out {dest}/cover.csv", shell=True)
+    subprocess.run(f"abd tile --raster {dir}/*.tif --zoom 17 --cover {dest}/cover.csv --config {config}/config.toml --out {dest}/images --format tif --keep_borders", shell=True)
+    subprocess.run(f"abd predict --cover {dest}/cover.csv --config {config}/config.toml --dataset {dest} --checkpoint {config}/neat-fullxview-epoch75.pth --out {dest}/predictions --metatiles --keep_borders", shell=True)
+    subprocess.run(f"abd vectorize --config {config}/config.toml --masks {dest}/predictions --out {dest}/buildings.geojson --type Building", shell=True)
+
+
+if __name__ == "__main__":
+    main()
--- ada_tools/random-scripts/jp2tiff.py
+++ ada_tools/random-scripts/jp2tiff.py
@@ -9,17 +9,27 @@ import click
 @click.option('--dir', help='directory with jp2 images')
 @click.option('--dest', default="", help='target directory for geotiffs')
 def main(dir, dest):
-    for path in Path(dir).rglob('*.JP2'):
-        path_jp2 = str(path)
-        if dest == "":
-            path_tif = str(path).replace('.JP2', '.tif')
-        else:
-            path_tif = os.path.join(dest, str(path.name).replace('.JP2', '.tif'))
-        print(path.name)
-        print(path_jp2)
-        print(path_tif)
 
-        subprocess.run(f"gdal_translate -b 1 -b 2 -b 3 -of GTiff -co TILED=YES {path_jp2} {path_tif}", shell=True)
+    for AOI in ['AOI1', 'AOI3', 'AOI2', 'AOI4', 'AOI5']:
+        os.makedirs(os.path.join(dir, AOI), exist_ok=True)
+        for prepost in ['pre', 'post']:
+            os.makedirs(os.path.join(dir, AOI, prepost+'-event'), exist_ok=True)
+            for path in Path(os.path.join(dir, prepost+'-event')).rglob('*.zip'):
+                if AOI in path.name:
+                    target_path = os.path.join(dir, AOI, prepost+'-event', str(path.name))
+                    subprocess.run(f"cp {str(path)} {target_path}", shell=True)
+                    subprocess.run(f"unzip {target_path} -d {os.path.join(dir, AOI, prepost+'-event')}", shell=True)
+
+            if dest == "":
+                dest = os.path.join(dir, AOI, prepost+'-event')
+            for path in Path(os.path.join(dir, AOI, prepost+'-event')).rglob('*.JP2'):
+                path_jp2 = str(path)
+                if dest == "":
+                    path_tif = str(path).replace('.JP2', '.tif')
+                else:
+                    path_tif = os.path.join(dest, str(path.name).replace('.JP2', '.tif'))
+                print(path.name)
+                subprocess.run(f"gdal_translate -of GTiff -co TILED=YES {path_jp2} {path_tif}", shell=True)
 
 
 if __name__ == "__main__":
